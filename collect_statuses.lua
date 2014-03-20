6e1c284b94ed74eb2f79484fb95e61e5e17a4695
Alexander Kushnarev <alexanderk@yandex-team.ru>
2014-03-20 05:25:02 +0000

    Add: #4 Support customizible counters. #6 predefined list of status codes

--- collect_statuses.lua
+++ collect_statuses.lua
@@ -1,20 +1,57 @@
-local stat_dict = ngx.shared.log_dict
-local status = ngx.status
-local curr_count = stat_dict:get(status)
-      
-if curr_count then
-  local new_count, err = stat_dict:incr(status, 1)
-  
-  if new_count then 
-    ngx.log(ngx.INFO, " Current counter for ", status, " is ", new_count)
-  else
-    ngx.log(ngx.ALERT, " Error while increase status counter ", err)
-  end
+local json = require("cjson")
+local stat_dict = ngx.shared.stat_dict
+local status = tostring(ngx.status)
+local location_group = ngx.var.stat_counter
 
-else
-      ngx.log(ngx.INFO, " Create new counter for ", status, " status ")
-      ok, err = stat_dict:set(status, 1)
-      if not ok then
-        ngx.log(ngx.ALERT, " Error while create status counter ", err)
-      end
+function init_location_group()
+  local location_group = {
+                          ["1xx"] = 0,
+                          ["2xx"] = 0,
+                          ["3xx"] = 0,
+                          ["4xx"] = 0,
+                          ["5xx"] = 0,
+                          ["xxx"] = 0
+                         }
+  return location_group
 end
+
+-- Set default group, if it's not defined by nginx variable
+if not location_group or location_group == "" then
+  location_group = 'other_locations'
+end
+
+-- Get serialized data from dict or create new
+local ser_stat = stat_dict:get("stat")
+if ser_stat then
+  stat = json.decode(ser_stat)
+else
+  stat = {["status"] = {[location_group] = init_location_group()}}
+end
+
+function get_status_code_class(status_code)
+  if     status_code:sub(1,1) == '1' then return "1xx"
+  elseif status_code:sub(1,1) == '2' then return "2xx"
+  elseif status_code:sub(1,1) == '3' then return "3xx"
+  elseif status_code:sub(1,1) == '4' then return "4xx"
+  elseif status_code:sub(1,1) == '5' then return "5xx"
+  else                                    return "xxx"
+  end
+end
+
+for i, gr in pairs(stat) do
+  if i == "status" then
+    status_class = get_status_code_class(status)
+
+    if not stat["status"][location_group] then
+      stat["status"][location_group] = init_location_group()
+    end
+
+    if stat["status"][location_group][status_class] then
+      old_count = stat["status"][location_group][status_class]
+      stat["status"][location_group][status_class] = old_count + 1
+    end
+  end
+end
+
+stat_dict:set("stat", json.encode(stat))
+ngx.log(ngx.ALERT, " stat ", json.encode(stat))
--- show_statuses_stat.lua
+++ show_statuses_stat.lua
@@ -1,25 +1,35 @@
 local json = require "cjson"
+local stat_dict = ngx.shared.stat_dict
 
-local stat_dict = ngx.shared.log_dict
-local data = {}
-local statuses = stat_dict:get_keys(20)
+ser_data = stat_dict:get("stat")
 
-for _, s in pairs(statuses) do 
-  data[s] = stat_dict:get(s)
+if not ser_data then
+  ngx.say("No data yet.")
+  ngx.exit(ngx.OK)
 end
 
 if ngx.req.get_headers()["Content-Type"] == "application/json" then
   -- Reply JSON if content type application/json
   ngx.header.content_type = "application/json"
-  ngx.say(json.encode(data)) 
+  ngx.say(ser_data)
+
 else
   -- Reply HTML in other ways
   local html_reply = {}
-  table.insert(html_reply, "<html><head><title>Nginx status statistic</title></head><body>")
-      
-  for s, c in pairs(data) do
-    table.insert(html_reply, "<p> status <b>" .. s .. "</b> has occurred " .. tostring(c) .. " times" .. "</p>" )
+  local data = json.decode(ser_data)
+
+  table.insert(html_reply, "<html><head><title>Nginx statistic</title></head><body>")
+  table.insert(html_reply, "<h1>Statuses:</h1>")
+
+  for group, value in pairs(data["status"]) do
+    table.insert(html_reply, "<h2>" .. group .. "</h2")
+    for s, c in pairs(value) do
+      table.insert(html_reply, "<b>" .. s .. "</b> has occurred "
+                               .. tostring(c) .. " times" .. "</p>")
+    end
   end
+
   table.insert(html_reply, "</body></html>")
+
   ngx.say(html_reply)
 end
