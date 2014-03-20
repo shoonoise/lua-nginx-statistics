6322c90230b9fca2c1bcb5eb990c2cdfba8844a7
Alexander Kushnarev <alexanderk@yandex-team.ru>
2014-03-20 12:09:07 +0400

    Add Dockerfile for test env. Fix test for docker env compatibility.

--- tests/tests.py
+++ tests/tests.py
@@ -1,8 +1,11 @@
 from urlparse import urljoin
 import requests
 import unittest
+import os
+import json
+import time
 
-HOST = "http://localhost"
+HOST = "http://%s" % os.environ['WEB_PORT_80_TCP_ADDR']
 
 
 class Base(unittest.TestCase):
@@ -14,7 +17,7 @@ class Base(unittest.TestCase):
     def _get_current_stat(name):
         reply = requests.get(urljoin(HOST, "status_stat"),
                              headers={'content-type': 'application/json'})
-        return reply.json().get(name)
+        return json.loads(reply.content).get(name)
 
     def _check_counter(fn):
         """Make a little magic to avoid code duplication.
@@ -23,6 +26,7 @@ class Base(unittest.TestCase):
             status = fn._testMethodName.split("_")[-1]
             current = Base._get_current_stat(status)
             requests.get(urljoin(HOST, status), allow_redirects=False)
+            time.sleep(1)
             new = Base._get_current_stat(status)
             unittest.TestCase.assertEqual(fn, new - current, 1,
                                           "Counter for status %s should be %s, but %s" % (status, current + 1, new))
