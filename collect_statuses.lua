23399e515ab10c635ca710307fdeef68f248c4a9
Alexander Kushnarev <alexanderk@yandex-team.ru>
2014-03-24 16:46:52 +0400

    Add `_total` field in reply as yandex-sysmon/nginx-lua-stats#4 suggested

--- collect_statuses.lua
+++ collect_statuses.lua
@@ -1,3 +1,4 @@
+module ("collect_statuses", package.seeall)
 local json = require("json")
 local stat_dict = ngx.shared.stat_dict
 local status = tostring(ngx.status)
--- show_statuses_stat.lua
+++ show_statuses_stat.lua
@@ -1,5 +1,6 @@
 local json = require "json"
 local stat_dict = ngx.shared.stat_dict
+local collect_module = require "collect_statuses"
 
 ser_data = stat_dict:get("stat")
 
@@ -8,15 +9,27 @@ if not ser_data then
   ngx.exit(ngx.OK)
 end
 
+data = json.decode(ser_data)
+
+-- Build _total field
+local _total = collect_module.init_location_group()
+for _, v in pairs(data["status"]) do
+  for s, c in pairs(v) do
+    _total[s] = _total[s] + c
+  end
+end
+data["status"]["_total"] = _total
+
+-- Build reply
+
 if ngx.req.get_headers()["Content-Type"] == "application/json" then
   -- Reply JSON if content type application/json
   ngx.header.content_type = "application/json"
-  ngx.say(ser_data)
+  ngx.say(json.encode(data))
 
 else
   -- Reply HTML in other ways
   local html_reply = {}
-  local data = json.decode(ser_data)
 
   table.insert(html_reply, "<html><head><title>Nginx statistic</title></head><body>")
   table.insert(html_reply, "<h1>Statuses:</h1>")
