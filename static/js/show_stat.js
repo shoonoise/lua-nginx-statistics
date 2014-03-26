baae91acf1199787b4335cea4b009386e86452a1
Alexander Kushnarev <alexanderk@yandex-team.ru>
2014-03-26 16:30:03 +0400

    Add html representation. Javascript get JSON by usual way and parse it into html.

---
+++ static/js/show_stat.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Alexander Kushnarev on 26.03.14.
+ */
+
+$( document ).ready(function() {
+
+    $.getJSON( "/stat", function( data ) {
+        var statistic = function () {
+            var main = "<h1>Nginx statistic page</h1>";
+            $.each(data, function (stat_type, stat_data) {
+                main += "<h2>" + stat_type + "</h2>";
+                $.each(stat_data, function (loc, values) {
+                    var counters_list = '<h3> In '+ loc +':</h3>';
+                    counters_list += "<ul>";
+                    $.each(values, function (s, count) {
+                        counters_list += "<li>" + s + " was occurred " + count + " times " + "</li>"
+                    });
+                    counters_list += "</ul>";
+                    main += counters_list
+                })
+            });
+            return main;
+        };
+        $("body").append(statistic);
+    });
+
+});
